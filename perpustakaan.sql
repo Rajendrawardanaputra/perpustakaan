7f616cebd018731a6ae560b78cb688bda8dea1d5
rajendra <101543629+Rajendrawardanaputra@users.noreply.github.com>
2022-09-14 16:46:50 -0700

    Add files via upload

---
+++ perpustakaan.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 14, 2022 at 03:40 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `perpustakaan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `nama_buku` text NOT NULL,
+  `deskripsi` text NOT NULL,
+  `foto` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `nama_buku`, `deskripsi`, `foto`) VALUES
+(1, 'tes', 'pp', 0x696d616765732e6a7067),
+(2, 'ttggtgt', 'fghjhgh', 0x66616365626f6f6b2e706e67),
+(3, 'buku kartun', 'hsdjfaklkead', 0x6b617274756e2e706e67);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_peminjaman_buku`
+--
+
+CREATE TABLE `detail_peminjaman_buku` (
+  `id_detail_peminjaman_buku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_peminjaman_buku`
+--
+
+INSERT INTO `detail_peminjaman_buku` (`id_detail_peminjaman_buku`, `id_peminjaman_buku`, `id_buku`, `qty`) VALUES
+(1, 1, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(20) NOT NULL,
+  `kelompok` varchar(20) NOT NULL,
+  `angkatan` int(120) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kelompok`, `angkatan`) VALUES
+(1, 'XI RPL 2', 'Rajendra Wardhana Pu', 0),
+(6, 'XI RPL 3', 'sucipto', 30),
+(7, 'XI RPL 3', 'sucipto', 30),
+(9, 'XI RPL 3', 'sucipto', 30),
+(10, 'XI RPL 2', 'sucipto', 30),
+(11, 'XI RPL 2', 'Panji derwanto', 31),
+(12, 'XI RPL 2', 'Rajendra Wardhana Pu', 29),
+(13, 'XI RPL 2', 'AJI', 29),
+(14, 'XI RPL 5', 'JINGGA', 10),
+(15, 'XI RPL 3', 'Panji derwanto', 31),
+(16, 'XI RPL 2', 'AJI', 10);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman_buku`
+--
+
+CREATE TABLE `peminjaman_buku` (
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `peminjaman_buku`
+--
+
+INSERT INTO `peminjaman_buku` (`id_peminjaman_buku`, `id_siswa`, `tanggal_pinjam`, `tanggal_kembali`) VALUES
+(1, 19, '2022-09-12', '2022-09-17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian_buku`
+--
+
+CREATE TABLE `pengembalian_buku` (
+  `id_pengembalian_buku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `denda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengembalian_buku`
+--
+
+INSERT INTO `pengembalian_buku` (`id_pengembalian_buku`, `id_peminjaman_buku`, `tanggal_pengembalian`, `denda`) VALUES
+(1, 1, '2022-09-12', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(11) NOT NULL,
+  `nama_siswa` varchar(100) NOT NULL,
+  `tanggal_lahir` date DEFAULT NULL,
+  `gender` enum('L','P') NOT NULL,
+  `alamat` text NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `id_kelas` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nama_siswa`, `tanggal_lahir`, `gender`, `alamat`, `username`, `password`, `id_kelas`) VALUES
+(17, 'Jingga', '2022-08-24', 'P', 'Tulungagung', 'jingga cantik banget', '6917e6014aa45687457b8e6cf3713515', 1),
+(19, 'jawara', '2022-09-24', 'P', 'asdfghjkl;', '@jendra', '2216017cb2a41f6f0366a260dd718062', 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`),
+  ADD UNIQUE KEY `nama_buku` (`nama_buku`) USING HASH;
+
+--
+-- Indexes for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD PRIMARY KEY (`id_detail_peminjaman_buku`),
+  ADD UNIQUE KEY `id_peminjaman_buku` (`id_peminjaman_buku`),
+  ADD UNIQUE KEY `id_buku` (`id_buku`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD PRIMARY KEY (`id_peminjaman_buku`),
+  ADD UNIQUE KEY `id_siswa` (`id_siswa`);
+
+--
+-- Indexes for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD PRIMARY KEY (`id_pengembalian_buku`),
+  ADD UNIQUE KEY `id_peminjaman_buku` (`id_peminjaman_buku`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD KEY `id_kelas` (`id_kelas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  MODIFY `id_detail_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  MODIFY `id_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+
+--
+-- AUTO_INCREMENT for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  MODIFY `id_pengembalian_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD CONSTRAINT `detail_peminjaman_buku_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_peminjaman_buku_ibfk_2` FOREIGN KEY (`id_peminjaman_buku`) REFERENCES `peminjaman_buku` (`id_peminjaman_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD CONSTRAINT `peminjaman_buku_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD CONSTRAINT `pengembalian_buku_ibfk_1` FOREIGN KEY (`id_peminjaman_buku`) REFERENCES `peminjaman_buku` (`id_peminjaman_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ tampil_kelas.php
@@ -0,0 +1,38 @@
+<?php include 'navbar.php';?>
+    <h3>Data Kelas</h3>
+    <table class="table table-hover table-striped">
+        <thead>
+            <tr>
+                <th>no</th>
+                <th>nama kelas</th>
+                <th>kelompok</th>
+                <th>angkatan</th>
+                <th>aksi</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php 
+            include "koneksi.php";
+            $qry_siswa=mysqli_query($conn,"select * from siswa join kelas on kelas.id_kelas=siswa.id_kelas");
+            $no=0;
+            while($data_siswa=mysqli_fetch_array($qry_siswa)){
+            $no++;?>
+    <tr>              
+    <td><?=$no?></td>
+    <td><?=$data_siswa['nama_kelas']?></td>
+     <td><?=$data_siswa['kelompok']?></td>
+     <td><?=$data_siswa['angkatan']?></td>
+     <td>
+    <a href="ubah_siswa.php?id_siswa=<?=$data_siswa['id_kelas']?>" class="btn btn-success">Ubah</a> | <a href="hapus.php?
+    id_siswa=<?=$data_siswa['id_kelas']?>" onclick="return 
+    confirm('Apakah anda yakin menghapus data ini?')" class="btn btn-danger">Hapus</a></td>
+    </tr>
+    <?php 
+ }
+    ?>
+</tbody>
+    </table>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <a href="tambah_kelas.php" class="btn btn-danger btn-lg rounded-pill">add kelas</a>
+</body>
+</html>
